25c8b3e3850f916a86890453c806a1faab9c1380
PetkoMihaylov <petko.h.mihaylov@gmail.com>
2026-03-03 20:25:31 +0200

    Preparing the server class.
    Setting up the server.

---
+++ Project/src/Main/Server.java
@@ -0,0 +1,75 @@
+package Main;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+
+    private ServerSocket serverSocket;
+
+    public Server() {
+
+    }
+
+    public void start()
+    {
+        try
+        {
+            System.out.println("The server is listening.");
+            try {
+                serverSocket = new ServerSocket(8080);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            while (true)
+            {
+                Socket client = serverSocket.accept();
+
+                Thread clientThread = new Thread(() ->
+                {
+                    System.out.println("Accepted client.");
+                    Scanner sc = null;
+                    PrintStream out = null;
+
+                    try
+                    {
+                        sc = new Scanner(client.getInputStream());
+                        out = new PrintStream(client.getOutputStream());
+                        //userMenu(sc, out);
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    finally
+                    {
+                        if (sc != null)
+                            sc.close();
+                        if (out != null)
+                            out.close();
+                    }
+                });
+
+                clientThread.start();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+        // For offline testing
+        //userMenu(new Scanner(System.in), System.out);
+    }
+
+
+
+
+
+}
---
+++ Project/src/Main/ServerMain.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class ServerMain
+{
+    public static void main(String[] args)
+    {
+        Server server = new Server();
+        server.start();
+    }
+}
